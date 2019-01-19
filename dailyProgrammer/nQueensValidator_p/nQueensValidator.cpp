09c2a5baddc1706a53de1a2729229ce137fb87ca
cslari <colarison@gmail.com>
2019-01-18 21:53:06 -0600

    first working copy

--- dailyProgrammer/nQueensValidator_p/nQueensValidator.cpp
+++ dailyProgrammer/nQueensValidator_p/nQueensValidator.cpp
@@ -15,74 +15,85 @@
 */
 
 #include<iostream>
-
+#include<array>
+#include<algorithm>
 using namespace std;
 
 // no queens can be in same row or diagonal,
 // 8 must be placed on 8x8 board to be valid.
 
+bool qcheck(array<int,8> queens);
 int main(){
-	// add & subtract index diff for diagonals,
-	// rows check number, columns inherently solved
-	// each entry need be only checked with subsequent entries?
-	 
+	array<bool,5> results;
+	
+	array<int,8> test1 = {4,2,7,3,6,8,5,1};
+	array<int,8> test2 = {2,5,7,4,1,8,6,3};
+	array<int,8> test3 = {5,3,1,4,2,8,6,3};
+	array<int,8> test4 = {5,8,2,4,7,1,3,6};
+	array<int,8> test5 = {4,3,1,8,1,3,5,2};
+	
+	results[0] = qcheck(test1);
+	results[1] = qcheck(test2);
+	results[2] = qcheck(test3);
+	results[3] = qcheck(test4);
+	results[4] = qcheck(test5);
 
+	// testing 
+	int testnum = 5;
+	array<string,5> solArray = {"true", "true", "false", "false", "false"};
+
+	for(int i=0; i<testnum; i++){
+		cout << "Test " << (i+1) << "\nexpected: " << solArray[i] << "\nresult: " << boolalpha << results[i] << "\n\n";
+	}
+	
 	return 0;
 }
 
-void qcheck(int[8] queens){
+bool qcheck(array<int,8> queens){
 	bool checkStatus = true;
 	// row check
-	int[8] sortQueens = queens;
+	array<int,8> sortQueens = queens;
 	sort(sortQueens.begin(), sortQueens.end());
 	for(int i=0; i<sortQueens.size()-1; i++){
 		if(sortQueens[i] == sortQueens[i+1]){
-			checkStatus = false;
-			break;
+			return (false);
 		}
 	}
 	
-//	if(queens[0] == (queens[1] + 1))
-//	if(queens[0] == (queens[1] - 1))
-//	if(queens[0] == (queens[2] + 2))
-//	if(queens[0] == (queens[2] - 2))
-//		...
-//	if(queens[0] == (queens[8] + 8))
-//	if(queens[0] == (queens[8] - 8))
+	// diagonal check
+
+	//	if(queens[0]+1 == queens[1]
+	//	if(queens[0]-1 == queens[1]
+	//	if(queens[0]+2 == queens[2] 
+	//	if(queens[0]-2 == queens[2] 
+	//		...
+	//	if(queens[0]+7 == queens[7] 
+	//	if(queens[0]-7 == queens[7]
 
 
-//	if(queens[1] == (queens[0] - 1)) // has already been checked in queens[0] cases
-//	if(queens[1] == (queens[2] + 1))
-//	if(queens[1] == (queens[2] - 1))
-//	if(queens[1] == (queens[8] + 7))
-//	if(queens[1] == (queens[8] - 7))
+	//	if(queens[1]-1 == queens[2] // has already been checked in queens[0] cases
+	//	if(queens[1]+1 == queens[2]
+	//	if(queens[1]+6 == queens[7]
+	//	if(queens[1]-6 == queens[7]
 
-//	formula:
-//	if(queens[index] == ( queens[checkIndex](+/-)(checkIndex - index) ) )
-//	
-//	for(int i=0; i<queens.size(); i++){
-//		index = i;
-//		checkIndex = queens.size();
-//		while(checkIndex > index){
-//			if(queens[index] == ( queens[checkIndex]+(checkIndex - index) ) ){
-//				// set check status to false & return
-//			}
-//			checkIndex--;
-//		}
-//	}
-//	if(checkStatus){
-//		// do diagonal check
-//		for(int i=0; i<queens.size(); i++){
-//			// refine logic here
-//			if((queens[i]+i) = queens[i+1]){
-//				checkStatus = false;
-//			}
-//			if((queens[i]-i) == queens[i+1]){
-
-//			}
-//		}
+	//	formula:
+	//	if(queens[index](+-)(checkIndex - index) ==  queens[checkIndex] 
+	//	
+	int checkIndex = 0;
+	for(int i=0; i<queens.size(); i++){
+		checkIndex = queens.size()-1;
+		while(checkIndex > i){
+			if( (queens[i]+(checkIndex - i)) ==  queens[checkIndex] ){
+				return(false);
+			}
+			else if( (queens[i]-(checkIndex - i)) ==  queens[checkIndex] ){
+				return(false);
+			}
+			checkIndex--;
+		}
 	}
+
 	
-	return;
+	return(true);
 }
 
